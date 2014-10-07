18a7c071c76d5afabf9ad3cc7d3066f8e365cd6e
Sabeen Syed <sabeen.syed@rackspace.com>
2014-10-07 13:43:15 -0500

    Added heat template for bike shed app

--- participants/code/case-1.py
+++ participants/code/case-1.py
@@ -4,23 +4,25 @@ from oslogger import log
 from db_node import DBServer
 from ha_node import HAProxyServer
 from web_node import WebServer
+import pdb
 
 if __name__ == "__main__":
     config = get_config()
 
     log.info("Creating web nodes.")
     nodes = []
+    pdb.set_trace()
     web_server = WebServer(config)
-    for name in ("server-1", "server-2"):
+    for name in ("sabeen-server-1", "sabeen-server-2"):
         nodes.append(web_server.create_node(name))
 
     log.info("Creating database node.")
     db_server = DBServer(config)
-    db_server.create_node("db")
+    db_server.create_node("sabeen-db")
 
     log.info("Creating haproxy load balancer node.")
     haproxy_server = HAProxyServer(config, nodes)
 
-    lb_node = haproxy_server.create_node("haproxy")
+    lb_node = haproxy_server.create_node("sabeen-haproxy")
     log.info("Access the load balancer at %s", lb_node.public_ips)
 
